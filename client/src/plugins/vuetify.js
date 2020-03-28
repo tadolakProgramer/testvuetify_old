a0c35c5803baa7fb713b82f20dd94f213965ef32
Tadeusz <tadolak@gmail.com>
2020-03-28 20:27:23 +0100

    modified Register card

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -3,7 +3,7 @@ import Vuetify from 'vuetify/lib';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@mdi/font/css/materialdesignicons.css'
-import colors from 'vuetify/lib/util/colors'
+//import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify,
     {iconfont: 'mdi'|| 'fa'}
@@ -14,7 +14,7 @@ export default new Vuetify({
         themes: {
             light: {
                 primary: '#4CAF50', // #E53935
-                secondary: colors.lightGreen, // #FFCDD2
+                secondary: '#d9ffb3',
                 accent: '#1565C0',
                 error: '#F44336',
                 info: '#00E676',
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,70 +1,93 @@
 <template>
-    <v-row
-            align="center"
-            justify="center"
-    >
-        <v-col
-                cols="12"
-                sm="8"
-                md="4"
-        >
-    <v-card>
-        <v-card-title color="green" class="headline">Logowanie</v-card-title>
-     <div class="pl-4 pr-4 pt-3 pb-3">
-         <v-text-field
-                 v-model="US_Name"
-                 :counter="6"
-                 :rules="[rules.required, rules.min]"
-                 hint="Minimum 6 znaków"
-                 label="Name"
-                 required
-         ></v-text-field>
-         <v-text-field
-                 v-model="US_PASS"
-                 :type="show1 ? 'text' : 'password'"
-                 hint="Minimum 8 znaków"
-                 :counter="6"
-                 :rules="[rules.required, rules.min]"
-                 :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                 @click:append="show1 = !show1"
-                 label="Hasło"
-         ></v-text-field>
-         <v-layout align-center justify-center>
-        <v-btn color="accent" dark
+    <v-layout>
+        <v-flex xs12 sm10 offset-sm1>
 
-          @click="register">Loguj
-            </v-btn>
-         </v-layout>
-      </div>
-    </v-card>
-    </v-col>
-    </v-row>
+            <v-card  class="pa-4" color="secondary">
+                <v-card-title color="green" class="headline">Nowy użytkownik</v-card-title>
+                    <v-row>
+                        <v-col cols="8" sm="4" md="4">
+                                <v-text-field
+                                        v-model="US_Name"
+                                        :rules="[rules.required]"
+                                        hint="Wpisz swoje imię"
+                                        label="Imię"
+                                        required
+                                ></v-text-field>
+                        </v-col>
+                            <v-col cols="8" sm="4" md="4">
+                                <v-text-field
+                                        v-model="US_SUER_NAME"
+                                        :rules="[rules.required]"
+                                        hint="Wpisz swoje nazwisko"
+                                        label="Nazwisko"
+                                        required
+                                ></v-text-field>
+                            </v-col>
+                    </v-row>
+                    <v-row>
+                        <v-col cols="8" sm="6" md="6">
+                                <v-text-field
+                                        v-model="US_PASS"
+                                        :type="show1 ? 'text' : 'password'"
+                                        hint="Minimum 8 znaków"
+                                        :counter="6"
+                                        :rules="[rules.required, rules.min]"
+                                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                        @click:append="show1 = !show1"
+                                        label="Hasło"
+                                ></v-text-field>
+                            </v-col>
+                        <v-col cols="8" sm="6" md="6">
+                            <v-text-field
+                                    v-model="US_PASS"
+                                    :type="show1 ? 'text' : 'password'"
+                                    hint="Minimum 8 znaków"
+                                    :counter="6"
+                                    :rules="[rules.required, rules.min]"
+                                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                                    @click:append="show1 = !show1"
+                                    label="Hasło"
+                            ></v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-layout align-center justify-center>
+                        <v-btn color="accent" dark
+                               @click="register">Loguj
+                        </v-btn>
+                    </v-layout>
+            </v-card>
+            </v-flex>
+        </v-layout>
+
 </template>
 
 <script>
-import AuthenticationService from '../services/AuthenticationService'
-export default {
-  data () {
-    return {
-        show1: false,
-        US_Name: '',
-        US_PASS: '',
-        rules: {
-            required: value => !!value || 'Required.',
-            min: v => v.length >= 6 || 'Min 8 characters'
+    import AuthenticationService from '../services/AuthenticationService'
+
+    export default {
+        data() {
+            return {
+                show1: false,
+                US_Name: '',
+                US_PASS: '',
+                US_SUER_NAME: '',
+                rules: {
+                    required: value => !!value || 'To pole nie może być puste.',
+                    min: v => v.length >= 6 || 'Min 6 znaków'
+                }
+            }
+        },
+        methods: {
+            async register() {
+                const response = await AuthenticationService.register({
+                    US_Name: this.US_Name,
+                    US_PASS: this.US_PASS,
+                    US_SUER_NAME: this.US_SUER_NAME
+                })
+                console.log(response.data)
+            }
         }
     }
-  },
-  methods: {
-    async register () {
-      const response = await AuthenticationService.register({
-        US_Name: this.US_Name,
-        US_PASS: this.US_PASS
-      })
-      console.log(response.data)
-    }
-  }
-}
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
