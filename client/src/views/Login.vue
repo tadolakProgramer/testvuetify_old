2abd2e7a563fbc218942414b7ae774f366aafdbb
Tadeusz <tadolak@gmail.com>
2020-04-06 20:21:43 +0200

    add alert to login

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,48 +1,83 @@
 <template>
-    <div class="masthead scroll-off-screen" >
-            <v-card
-                    class="pa-6 ma-auto"
-                    max-width="344"
+    <div class="masthead scroll-off-screen">
+        <v-card
+                class="pa-6 ma-auto"
+                max-width="344"
+                outlined
+                elevation="15"
+                color="transparent"
+        >
+            <v-text-field
+                    v-model="US_LOGIN"
+                    hint="Wpisz swoje Login"
+                    label="Login"
                     outlined
-                    elevation="15"
-                    color="transparent"
-            >
-                <v-text-field
-                        v-model="US_LOGIN"
-                        hint="Wpisz swoje Login"
-                        label="Login"
-                        outlined
-                ></v-text-field>
-                <v-text-field
-                        v-model="US_PASS"
-                        :type="show1 ? 'text' : 'password'"
-                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                        @click:append="show1 = !show1"
-                        label="Hasło"
-                        outlined
-                ></v-text-field>
-
-                <v-card-actions>
-                    <v-layout align-center justify-center>
+            ></v-text-field>
+            <v-text-field
+                    v-model="US_PASS"
+                    :type="show1 ? 'text' : 'password'"
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="show1 = !show1"
+                    label="Hasło"
+                    outlined
+            ></v-text-field>
+            <v-alert
+                    :value="alert"
+                    type="error"
+                    color="red"
+                    outlined
+                    elevation="2"
+                    transition="scale-transition"
+            >{{LoginError}}
+            </v-alert>
+            <v-card-actions>
+                <v-layout align-center justify-center>
                     <v-btn
                             class="ma-2"
                             outlined
-                    >
-                        <v-icon left>mdi-account-outline</v-icon>Loguj
-                        </v-btn>
-                    </v-layout>
-                </v-card-actions>
-            </v-card>
+                            @click="login">
+                        <v-icon left>mdi-account-outline</v-icon>
+                        Loguj
+                    </v-btn>
+                </v-layout>
+            </v-card-actions>
+        </v-card>
     </div>
 </template>
 
 <script>
+    import AuthenticationService from '../services/AuthenticationService'
+
     export default {
         data() {
             return {
+                LoginError: null,
+                alert: false,
                 US_LOGIN: '',
                 US_PASS: '',
                 show1: false,
+            }
+        },
+        methods: {
+            async login() {
+                try {
+                    await AuthenticationService.login({
+                        US_LOGIN: this.US_LOGIN,
+                        US_PASS: this.US_PASS
+                    })
+                    this.$router.push('/')
+                } catch (LoginError) {
+                    this.LoginError = LoginError.response.data.LoginError;
+                    this.alert = true
+
+                }
+            }
+        },
+        watch: {
+            US_LOGIN: function () {
+                if (this.alert) {
+                    this.alert = false
+                }
             }
         }
     }
--- server/controllers/AuthenticationController.js
+++ server/controllers/AuthenticationController.js
@@ -1,19 +1,19 @@
 const Users = require('../models/users');
 const Hale = require('../models/hale');
 
-module.exports ={
-    async register (req, res) {
+module.exports = {
+    async register(req, res) {
         try {
-            console.log('reqbody ',req.body);
-            const { count, rows } = await Users.findAndCountAll({where: {US_LOGIN: req.body.US_LOGIN}});
-            console.log(count);
-            if (count<1){
-                await Users.create(req.body);
-                res.status(400).send({"ServerMessageOK":"Jest OK!!!"})}
-            else {
-                res.status(400).send({"ServerMessageE":"Taki login juz istnieje!!!"});
-            }
 
+            const {count, rows} = await Users.findAndCountAll(
+                {where: {US_LOGIN: req.body.US_LOGIN}});
+            console.log(count);
+            if (count < 1) {
+                await Users.create(req.body);
+                res.status(400).send({"ServerMessageOK": "Jest OK!!!"})
+            } else {
+                res.status(400).send({"ServerMessageE": "Taki login juz istnieje!!!"});
+            }
         } catch (err) {
             console.log('error', err);
             res.status(400).send({
@@ -21,23 +21,28 @@ module.exports ={
             })
         }
     },
-    async login (req, res) {
+    async login(req, res) {
         try {
+            console.log('reqbody ', req.body);
             const {US_LOGIN, US_PASS} = req.body;
             const user = await Users.findOne({
                 where: {
-                    US_LOGIN : US_LOGIN
+                    US_LOGIN: US_LOGIN
                 }
             })
-            if (!user){
+            if (!user) {
+                res.status(400).send({"LoginError":"Nie ma takiego urzytkownika"});
                 console.log('no user');
             }
-        }
-        catch (error){
+            if (user){
+                console.log('user OK!')
+            }
+        } catch (error) {
             console.log('login error');
+            res.send('dupa');
         }
     },
-    async getHale (req, res) {
+    async getHale(req, res) {
         await Hale.findAll()
             .then(hale => res.send(hale))
             .catch(error => console.log(error));
