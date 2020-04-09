5159eb2ae487490c11dfe17db0e24f35041b6316
Tadeusz <tadolak@gmail.com>
2020-04-09 21:04:44 +0200

    add passport and jwt token fix

--- server/controllers/AuthenticationController.js
+++ server/controllers/AuthenticationController.js
@@ -14,74 +14,66 @@ function jwtSignUser (user) {
     })
 }
 
-async function isMatchPassword (PASS, Hash){
-    await bcrypt.compare(PASS, Hash, function(err, result) {
-       return result;
-    })
-}
-
 module.exports = {
     async register(req, res) {
         try {
-
-            const {count, rows} = await Users.findAndCountAll(
-                {where: {US_LOGIN: req.body.US_LOGIN}});
+            const {US_LOGIN} = await req.body;
+            const {count, row} = await Users.findAndCountAll({
+                where: {US_LOGIN: US_LOGIN}
+            })
             if (count < 1) {
-                bcrypt.hash(req.body.US_PASS, saltRounds).then(function ( hash)  {
-                 const user = Users.create({
-                        US_Name : req.body.US_Name,
-                        US_SUER_NAME: req.body.US_SUER_NAME,
-                        US_LOGIN :  req.body.US_LOGIN,
-                        US_PASS : hash,
+                const hash = await bcrypt.hash(req.body.US_PASS, saltRounds);
+                const user = Users.create({
+                    US_Name: req.body.US_Name,
+                    US_SUER_NAME: req.body.US_SUER_NAME,
+                    US_LOGIN: req.body.US_LOGIN,
+                    US_PASS: hash,
                     US_PROFESJA: req.body.US_PROFESJA,
-                    Hala_ID: req.body.Hala_ID});
-                    const userJson = user.toJSON();
-                    res.send({
-                        user : userJson,
-                        token: jwtSignUser(userJson)
-                    });
-                    res.status(400).send({"ServerMessageOK": "Jest OK!!!"})
+                    Hala_ID: req.body.Hala_ID
                 });
-            } else {
+                const userJson = user.toJSON();
+                res.send({
+                    user: userJson,
+                    token: jwtSignUser(userJson)
+                })
+            }
+            else{
                 res.status(403).send({"ServerMessageE": "Taki login juz istnieje!!!"});
             }
-        } catch (err) {
-            console.log('error', err);
-            res.status(400).send({
-                error: 'This Login account is already in use.'
-            })
+        }
+        catch (e) {
         }
     },
     async login(req, res) {
-        try {
-            console.log('reqbody1 ', req.body);
-            const {US_LOGIN, US_PASS} = req.body;
-            const user = await Users.findOne({
+        try{
+            const {US_PASS, US_LOGIN} = await req.body;
+            const userData = await Users.findOne({
                 where: {
                     US_LOGIN: US_LOGIN
                 }
-            });
-            if (!user) {
-                res.status(403).send({"LoginError":"Nie ma takiego urzytkownika"});
-                console.log('no user');
-            }
-            const  hash = await bcrypt.hash(req.body.US_PASS, saltRounds);
-            const validPass = isMatchPassword(hash, user.US_PASS);
-            if (!validPass){
-                res.status(403).send({"LoginError":"Nie pamiętasz hasła"});
-            }
-            const userJson = user.toJSON();
-            res.send({
-                user : userJson,
-                token: jwtSignUser(userJson)
-            });
-            console.log(userJson)
-
-        } catch (err) {
-            console.log(err)
-            res.status(403).send({
-                error: 'An error has occured trying to log in'
             })
+            if (!userData){
+                res.status(403).send({"LoginError": "Nie ma takiego urzytkownika"});
+            }
+            else{
+                const validPass = await bcrypt.compare(US_PASS, userData.US_PASS)
+                if (validPass) {
+                    const userJson = userData.toJSON();
+                    res.send({
+                        user: userJson,
+                        token: jwtSignUser(userJson)
+                    })
+                }
+            else{
+                    res.status(403).send({"LoginError": "Nie pamiętasz hasła??"});
+                }
+            }
+        }
+        catch (err) {
+            console.log(err)
+            res.status(403).send(
+            {"LoginError": 'An error has occured trying to log in'})
+
         }
     },
     async getHale(req, res) {
