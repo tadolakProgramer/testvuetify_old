21324e919ab7db5fdb966372397f7a436ac3be61
Tadeusz <tadolak@gmail.com>
2020-05-13 11:27:28 +0200

    add icons to buttons

--- server/app.js
+++ server/app.js
@@ -3,11 +3,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const dns = require('dns');
 
 const server = require('http').createServer();
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+    next();
+
+});
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
@@ -18,5 +26,9 @@ require('./routers/router')(app);
 require('./routers/notification')(app);
 require('./routers/failure')(app)
 
-server.listen(port, () => console.log(`Listening on ${port}`));
+dns.lookup('localhost',(err, adresess, family) => console.log('Adresss:',adresess, family ))
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+
+});
 //app.listen(803);
